3f892325c819afc1948b14d2f027c0de2aa59c0b
Shutdownnn <pittaya33995@gmail.com>
2023-01-17 16:22:35 +0700

    Initial commit

---
+++ app/src/main/java/com/example/calv2/MainActivity.kt
@@ -0,0 +1,138 @@
+package com.example.calv2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        var str:String
+        var ss:String
+        one.setOnClickListener {
+            ss = "1"
+            if (ans.text.toString() == "0")
+                ans.setText(ss)
+            else {
+                str = ans.text.toString().plus(ss)
+                ans.setText(str)
+            }
+        }
+
+        two.setOnClickListener {
+            ss = "2"
+            if (ans.text.toString() == "0")
+                ans.setText(ss)
+            else {
+                str = ans.text.toString().plus(ss)
+                ans.setText(str)
+            }
+        }
+
+        three.setOnClickListener {
+            ss = "3"
+            if (ans.text.toString() == "0")
+                ans.setText(ss)
+            else {
+                str = ans.text.toString().plus(ss)
+                ans.setText(str)
+            }
+        }
+
+        four.setOnClickListener {
+            ss = "4"
+            if (ans.text.toString() == "0")
+                ans.setText(ss)
+            else {
+                str = ans.text.toString().plus(ss)
+                ans.setText(str)
+            }
+        }
+
+        five.setOnClickListener {
+            ss = "5"
+            if (ans.text.toString() == "0")
+                ans.setText(ss)
+            else {
+                str = ans.text.toString().plus(ss)
+                ans.setText(str)
+            }
+        }
+
+        six.setOnClickListener {
+            ss = "6"
+            if (ans.text.toString() == "0")
+                ans.setText(ss)
+            else {
+                str = ans.text.toString().plus(ss)
+                ans.setText(str)
+            }
+        }
+
+        seven.setOnClickListener {
+            ss = "7"
+            if (ans.text.toString() == "0")
+                ans.setText(ss)
+            else {
+                str = ans.text.toString().plus(ss)
+                ans.setText(str)
+            }
+        }
+
+        eight.setOnClickListener {
+            ss = "8"
+            if (ans.text.toString() == "0")
+                ans.setText(ss)
+            else {
+                str = ans.text.toString().plus(ss)
+                ans.setText(str)
+            }
+        }
+
+        nine.setOnClickListener {
+            ss = "9"
+            if (ans.text.toString() == "0")
+                ans.setText(ss)
+            else {
+                str = ans.text.toString().plus(ss)
+                ans.setText(str)
+            }
+        }
+
+        zero.setOnClickListener {
+            ss = "0"
+            if (ans.text.toString() == "0")
+                ans.setText(ss)
+            else {
+                str = ans.text.toString().plus(ss)
+                ans.setText(str)
+            }
+        }
+
+        del.setOnClickListener {
+            if (ans.text.length != 1) {
+                str = ans.text.toString().dropLast(1)
+                ans.setText(str)
+            }
+            else
+                str = "0"
+                ans.setText(str)
+        }
+
+        clear.setOnClickListener {
+            ans.setText("0")
+        }
+
+        plus.setOnClickListener {
+            var n1 = ans.text.toString()
+            ans.text = n1
+            str = ""
+            ans.setText(str)
+            var result = n1.toDouble() + str.toDouble()
+            ans.setText("$result")
+        }
+
+    }
+}
